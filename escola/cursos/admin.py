be6261f92fcff79eb08238c44496dc0d722125fb
Marcus Aldrey <marcusaldrey@gmail.com>
2021-12-16 13:24:22 -0300

    create models and migrations

--- escola/cursos/admin.py
+++ escola/cursos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from cursos.models import Avaliacao, Curso
+
 # Register your models here.
+
+@admin.register(Curso)
+class cursoAdmin(admin.ModelAdmin):
+  list_display = ('titulo', 'url', 'criacao', 'atualizacao', 'ativo')
+  
+  
+@admin.register(Avaliacao)
+class avaliacaoAdmin(admin.ModelAdmin):
+  list_display = ('curso', 'nome', 'email', 'avaliacao', 'criacao', 'atualizacao', 'ativo')
