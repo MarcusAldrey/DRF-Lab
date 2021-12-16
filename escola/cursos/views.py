dddebaed5ca5388d0582f440edde33e27dca45f9
Marcus Aldrey <marcusaldrey@gmail.com>
2021-12-16 16:19:01 -0300

    add views and urls

--- escola/cursos/views.py
+++ escola/cursos/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import request
+from rest_framework.serializers import Serializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
 
-# Create your views here.
+
+class CursoAPIView(APIView):
+  """[summary]
+
+  Args:
+      APIView ([type]): [description]
+  """
+  
+  def get(self, request):
+    cursos = Curso.objects.all()
+    serializer = CursoSerializer(cursos, many=True)
+    return Response(serializer.data)
+  
+  
+class AvaliacaoAPIView(APIView):
+  """"
+  API de Avaliações
+  """
+  
+  def get(self, request):
+    avaliacoes = Avaliacao.objects.all()
+    serializer = AvaliacaoSerializer(avaliacoes, many=True)
+    return Response(serializer.data)
+  
